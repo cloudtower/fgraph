86e8beb51b23a2e3cbce48de77355bb0ce040419
Lion <stegerl@in.tum.de>
2024-11-23 19:43:59 +0100

    Link strengths are automatically set to at least 1

--- static/main.js
+++ static/main.js
@@ -218,6 +218,8 @@ function switch_mode(mode) {
 // Data manipulation functions
 
 function add_link(source, target, value, type) {
+    value = Math.max(10.0, value);
+
     jsonData.links.push({
         "source": source,
         "target": target,
@@ -277,7 +279,7 @@ function edit_link(event, form) {
     data = new FormData(form);
     link_edit = jsonData.links[data.get("id")]
     link_edit["type"] = data.get("type");
-    link_edit["value"] = data.get("value") / 10;
+    link_edit["value"] = Math.max(1.0, data.get("value") / 10);
     link_edit["dist"] = 1 / data.get("value") * 3000;
     link_edit["comment"] = data.get("comment");
 
